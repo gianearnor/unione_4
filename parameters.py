6a5fb31ae5e07c444f14b9110a6710013fd16588
g.molteni <g.molteni@list-group.com>
2022-03-29 22:04:20 +0200

    - i parametri sono stati spostati nel file parameters.py

---
+++ parameters.py
@@ -0,0 +1,22 @@
+import os
+import random
+
+# PARAMETERS
+N_elementi_da_ispezionare = [4, 2]
+DIFF_FACT = 4
+CUTOFF = 400
+OBJ_SIZE_min = 30
+OBJ_SIZE_max = 100
+OBJ_SIZE_stp = 1
+OBJ_SIZE_mean = (OBJ_SIZE_min + OBJ_SIZE_max) // 2
+OBJ_SIZE_num = (OBJ_SIZE_max - OBJ_SIZE_min)/OBJ_SIZE_stp
+OBJ_COL_min = 30
+OBJ_COL_max = 200
+OBJ_COL_stp = 1
+OBJ_COL_mean = (OBJ_COL_min + OBJ_COL_max) // 2
+OBJ_COL_num = (OBJ_COL_max - OBJ_COL_min)/OBJ_COL_stp
+Tempo_scritte = 1000
+FPS = 60
+ID_NUMBER = random.randrange(1000, 9999, 1)
+userhome = os.path.expanduser('~')
+user = os.path.split(userhome)[-1]
--- unione_4.py
+++ unione_4.py
@@ -7,27 +7,9 @@ import sys
 import os
 from block import Block
 from utilities_func import render_text
+from parameters import *
 
 ctypes.windll.user32.SetProcessDPIAware()
-# PARAMETERS
-N_elementi_da_ispezionare = [4, 2]
-DIFF_FACT = 4
-CUTOFF = 400
-OBJ_SIZE_min = 30
-OBJ_SIZE_max = 100
-OBJ_SIZE_stp = 1
-OBJ_SIZE_mean = (OBJ_SIZE_min + OBJ_SIZE_max) // 2
-OBJ_SIZE_num = (OBJ_SIZE_max - OBJ_SIZE_min)/OBJ_SIZE_stp
-OBJ_COL_min = 30
-OBJ_COL_max = 200
-OBJ_COL_stp = 1
-OBJ_COL_mean = (OBJ_COL_min + OBJ_COL_max) // 2
-OBJ_COL_num = (OBJ_COL_max - OBJ_COL_min)/OBJ_COL_stp
-Tempo_scritte = 1000
-FPS = 60
-ID_NUMBER = random.randrange(1000, 9999, 1)
-userhome = os.path.expanduser('~')
-user = os.path.split(userhome)[-1]
 '''
 results = pd.DataFrame({'N': [], 'x0': [], 'scelta': [],'dim_iniz': [],'handling_time': [],
                                 'time_0': [], 'click_time': [], 'exit_time': []})
